599af1dffbb9808e7b8c3ffb7516e90885b8555d
FaizaFarooq23 <faizafarooq2325@gmail.com>
2023-07-07 01:30:45 +0500

    Card created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import Topbar from './component/Topbar';
 import Baselayout from './component/Baselayout';
 
 function App() {
   return (
     <Baselayout>
-    <Topbar/>
     </Baselayout>
   );
 }
