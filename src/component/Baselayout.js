f630dd6c8b22dda3016c108e5aa5fa970baeb026
FaizaFarooq23 <faizafarooq2325@gmail.com>
2023-07-06 17:43:42 +0500

    footer added

--- src/component/Baselayout.js
+++ src/component/Baselayout.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Topbar from './Topbar'
+import Footer from './Footer'
 
 export default function Baselayout() {
-  return (
-    <>
-    <div className='flex justify-between background-image h-screen w-screen '>
-    <Topbar/>
-    </div>
-    </>
-  )
+    return (
+        <>
+            <div className='flex flex-col justify-between background-image h-screen w-screen '>
+                <Topbar />
+                <Footer/>
+            </div>
+        </>
+    )
 }
